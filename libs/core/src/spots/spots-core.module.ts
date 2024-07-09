6d5dccc087058e633eba11c09f419080941b1365
Jairo Koning <jairo.koning@gmail.com>
2024-07-09 13:03:10 +0000

    feat: add app partner1

--- libs/core/src/spots/spots-core.module.ts
+++ libs/core/src/spots/spots-core.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { SpotsService } from './spots.service';
 
 @Module({
+  exports: [SpotsService],
   providers: [SpotsService],
 })
 export class SpotsCoreModule {}
